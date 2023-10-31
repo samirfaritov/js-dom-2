350b09564fb0fd458495accbcaa79c6c00679b34
samirfaritov <samirfaritov6@gmail.com>
2023-10-31 17:24:40 +0500

    js dom 2 click mouseenter dblclick

---
+++ main.js
@@ -0,0 +1,51 @@
+// let txt = document.querySelector('#text')
+// let btn = document.querySelector('#btn')
+
+
+// // btn.onlick = () => {
+// //     console.log("Salom");
+// // }
+
+
+// btn.addEventListener('click', () => {
+    //     // console.log("Salom");
+    //     active = !active
+    
+    //     if (active) {
+        //         txt.innerText = 'hello'
+        //     } else {
+            //         txt.innerText = 'salom'
+            //     }
+            
+            //     // console.log(active);
+            // })
+            
+            
+//             let active = false
+            
+
+
+
+// box.addEventListener('click', () => {
+    //     active = !active
+    //     if (active) {
+        //         box.style.backgroundColor = 'blue'
+        //         box.style.borderRadius = '50%'
+        //     } else {
+            //         box.style.borderRadius = '0'
+            //         box.style.backgroundColor = 'white'
+            //     }
+            // })
+            
+            // let inp = document.addEventListener('keyup', (event) => {
+                //     let value = event.target.value
+                //     console.log(value);
+                // })
+
+                // let box = document.querySelector('.box')
+
+                // box.addEventListener("mouseEnter", () => {
+                //     box.style.backgroundColor = 'blue'
+                // })
+
+                
